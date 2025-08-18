d036d78fcf237a3089ba19e810f10467ef4a64c4
ShurikenStar <139156894+ShurikenStar@users.noreply.github.com>
2025-08-18 13:46:19 +0530

    Add files via upload

---
+++ scratch_1.py
@@ -0,0 +1,3 @@
+a=(input("Enter your first number "))
+b=(input("Enter your last name "))
+print("Hello",a+" "+b,"! Welcome to the Python program")
---
+++ scratch_2.py
@@ -0,0 +1,6 @@
+a=int(input("input the first number "))
+b=int(input("input the second number "))
+print("Addition",a+b)
+print("Subtraction",a-b)
+print("Multiplication",a*b)
+print("Division",a/b)
